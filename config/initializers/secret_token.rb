4219f9067b6c55e5160c810326f3c290109ab2b9
Constantin <mitides.constantin@gmail.com>
2013-05-06 12:22:59 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NYCdata::Application.config.secret_token = '60adf6bde12b97fe67caf660bb6660085815b0632bf62e429157c467d9571c811b02640bbdf5f12b0af31984da0a2824e5775168423ebcdd9ada813d6f70834d'
