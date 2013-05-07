9b0a2e32032d7857cbdf687890a4fa1c5fb9e736
Johnny Bowman <JohnnyBowman@gmail.com>
2013-05-07 15:43:54 -0400

    removed transit and crime migration files

--- db/migrate/20130506215443_create_transit_datapoints.rb
+++
@@ -1,9 +0,0 @@
-class CreateTransitDatapoints < ActiveRecord::Migration
-  def change
-    create_table :transit_datapoints do |t|
-      t.integer :zipcode_id
-
-      t.timestamps
-    end
-  end
-end
