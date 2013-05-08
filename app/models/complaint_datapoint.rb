eef044353639d00768322a5d2ac1dedb864a9291
Johnny Bowman <JohnnyBowman@gmail.com>
2013-05-08 09:21:19 -0400

    added json file to assets and parsed json file in compltaintDatapoint.rb model

--- app/models/complaint_datapoint.rb
+++ app/models/complaint_datapoint.rb
@@ -1,5 +1,28 @@
 class ComplaintDatapoint < ActiveRecord::Base
   attr_accessible :zipcode_id
 
+  
+		def get_zipcode_data
+			file = File.join(Rails.root, 'app', 'assets', 'NYCOpenData', 'noise_complaints.json')
+			json = File.read(file)
+			noise_complaints = JSON.parse(json)
+			noise_complaints = noise_complaints["data"]
+			noise_complaints.each do |complaint|
+				complaint = ComplaintDatapoint.new
+				complaint.type = complaint[14]
+				complaint.zipcode = complaint[16]
+				complaint.latitude = complaint[57]
+				complaint.longitude = complaint[58]
+				complaint.date = complaint[9]
+				complaint.address = complaint[17]
+				complaint.save
+			end
+
+
+
   belongs_to :zipcode
+
+
+
+
 end
