c70b4522d1ea29d108545f03e9642f6a2602fc7b
Kevin Bajorin <KBajorin@gmail.com>
2013-05-08 09:17:34 -0400

    adding zipcode_step file

---
+++ features/step_definitions/zipcode_steps.rb
@@ -0,0 +1,7 @@
+Given(/^I am on the zipcode page$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I should see zipdcode (\d+) calls$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
