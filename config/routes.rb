96dccc2160a54137d7f50a96ae3d32666c8cd9a9
Johnny Bowman <JohnnyBowman@gmail.com>
2013-05-12 16:35:16 -0400

    added beginnings of grid framework to zipcodes show page

--- config/routes.rb
+++ config/routes.rb
@@ -62,7 +62,7 @@ resources :comments, :only => [:index, :create, :destroy]
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'zipcodes#index'
+  root :to => 'neighborhoods#index'
 
   # See how all your routes lay out with "rake routes"
 
